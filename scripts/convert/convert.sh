9ae25913c674df78c2213eddbe50406b8eecebf5
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-08-01 16:10:51 +0200

    Refactor convert script

--- scripts/convert/convert.sh
+++ scripts/convert/convert.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-pdf_input=$1
-ps_output=${pdf_input%.*}.ps
-txt_output=${pdf_input%.*}.txt
-pdfa_output=${pdf_input%.*}_a.pdf
+input_file=$1
+file_name="$(basename -- $input_file)"
+output_path="output/"
+
+ps_output="${output_path}${file_name}.ps"
+txt_output="${output_path}${file_name}.txt"
+pdfa_output="${output_path}${file_name}_a.pdf"
+
+mkdir -p output
 
 # Convert to PostScript
-pdftops ${pdf_input} ${ps_output}
+pdftops "${input_file}" "${ps_output}"
 
 # Convert to text
-pdftotext -enc UTF-8 ${pdf_input} ${txt_output}
+pdftotext -enc UTF-8 "${input_file}" "${txt_output}"
 
 # Convert to PDF/A
-gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=${pdfa_output} ${ps_output}
+gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile="${pdfa_output}" "${ps_output}"
