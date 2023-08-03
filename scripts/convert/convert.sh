3c631c7d2700b12013d80b31a55faa81db94fcee
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-08-03 11:55:55 +0200

    Refactor convert.sh

--- scripts/convert/convert.sh
+++ scripts/convert/convert.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
 
-input_file=$1
-file_name="$(basename -- $input_file)"
-output_path="output/"
+INPUT_FILE=""
+OUTPUT_FILE=""
+OUTPUT_FORMAT=""
 
-ps_output="${output_path}${file_name}.ps"
-txt_output="${output_path}${file_name}.txt"
-pdfa_output="${output_path}${file_name}_a.pdf"
+_parse_options() {
+  while getopts ":hi:o:f:" option; do
+    case $option in
+      h)
+        _help
+        exit;;
+      i)
+        INPUT_FILE=$OPTARG;;
+      o)
+        OUTPUT_FILE=$OPTARG;;
+      f)
+        OUTPUT_FORMAT=$OPTARG;;
+      \?)
+        echo "Error: Invalid option"
+        _help
+        exit;;
+     esac
+  done
+}
 
-mkdir -p output
+_help() {
+  echo "Usage: ./convert-to-text.sh"
+  echo ""
+  echo "Available options:"
+  echo "-i                Input file"
+  echo "-o                Output file"
+  echo "-f                Output file format (txt, pdfa, ps)"
+  echo "-h                Display help"
+}
 
-# Convert to PostScript
-pdftops "${input_file}" "${ps_output}"
+_parse_options "$@"
 
-# Convert to text
-pdftotext -enc UTF-8 "${input_file}" "${txt_output}"
+case $OUTPUT_FORMAT in
+  txt)
+    # Convert to text
+    pdftotext -enc UTF-8 "${INPUT_FILE}" "${OUTPUT_FILE}"
+    ;;
 
-# Convert to PDF/A
-gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile="${pdfa_output}" "${ps_output}"
+  ps)
+    # Convert to PostScript
+    pdftops "${INPUT_FILE}" "${OUTPUT_FILE}"
+    ;;
+
+  pdfa)
+    # Convert to PDF/A
+    gs -dPDFA -dBATCH -dNOPAUSE -dNOOUTERSAVE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile="${OUTPUT_FILE}" "${INPUT_FILE}"
+    ;;
+
+  *)
+    echo "Use one of the following formats: txt, ps, pdfa"
+    ;;
+esac
