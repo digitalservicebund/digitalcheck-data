e58bd9a8518af7a7fe0f167c1db2d6ca847c7b5c
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-08-07 15:25:20 +0200

    Add script to extract data from pdf using pdfplumber
    Read radio button and checkboxes

--- scripts/extract-python/README.md
+++ scripts/extract-python/README.md
@@ -2,7 +2,8 @@
 
 ## Extract data from PDF
 
-The `extract-data-from-pdf.py` script can be used to extract data from PDF files.
+The `extract-data-from-pdf.py` script can be used to extract data (e.g. answers on radio buttons and checkboxes) 
+from PDF files.
 
 ### Install
 
@@ -13,5 +14,21 @@ pipenv install
 ### Usage
 
 ```
- pipenv run python extract-data-from-pdf.py <path-to-pdf>
+ pipenv run python extract-data-from-pdf.py -i <input-file> -o <output-file> 
+```
+
+## Extract text from PDF
+
+The `extract-text-from-pdf.py` script can be used to extract text from PDF files.
+
+### Install
+
+```
+pipenv install
+```
+
+### Usage
+
+```
+ pipenv run python extract-text-from-pdf.py <path-to-pdf>
 ```
--- scripts/extract-python/extract-data-from-pdf.py
+++ scripts/extract-python/extract-data-from-pdf.py
@@ -1,36 +1,76 @@
-from pypdf import PdfReader
 import os
 import json
 import sys
+import argparse
+import pdfplumber
 
-# Create output directory if not existing
-outputPath = "output"
-isExist = os.path.exists(outputPath)
-if not isExist:
-    os.makedirs(outputPath)
+answers = {
+    0: 'Ja',
+    1: 'Nein',
+    2: 'Teilweise',
+    3: 'Nicht relevant',
+}
 
-# Read file from arguments
-if len(sys.argv) < 2:
-    print("At least one argument expected.")
-    sys.exit()
-inputFile = sys.argv[1]
 
-# Create reader for file
-reader = PdfReader(inputFile)
-outputFilename = os.path.basename(inputFile)
-outputFilename = outputFilename[:-4]
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Just an example",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--input-file", help="Input file")
+    parser.add_argument("-o", "--output-file", help="Output file")
+    args = parser.parse_args()
+    return vars(args)
 
-# Extract text
-textFile = open(outputPath + "/" + outputFilename + ".txt", "w")
-for page in reader.pages:
-    textFile.write(page.extract_text())
-textFile.close()
 
-# Extract fields as JSON
-jsonFile = open(outputPath + "/" + outputFilename + ".json", "w")
-fields = reader.get_form_text_fields()
-jsonField = json.dumps(fields)
-jsonFile.write(jsonField)
-jsonFile.close()
+def create_output_directory(output_file):
+    output_path = os.path.dirname(output_file)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+
+def parse_pdf(input_file, output_file):
+    with pdfplumber.open(input_file) as pdf:
+        data = {
+            'principle1Radio': extract_answer_on_page(pdf.pages[12]),
+            'principle2Radio': extract_answer_on_page(pdf.pages[13]),
+            'principle3Radio': extract_answer_on_page(pdf.pages[14]),
+            'principle4Radio': extract_answer_on_page(pdf.pages[15]),
+            'principle5Radio': extract_answer_on_page(pdf.pages[16])
+        }
+
+        json_file = open(output_file, "w")
+        json_field = json.dumps(data)
+        json_file.write(json_field)
+        json_file.close()
+
+
+def extract_answer_on_page(page):
+    radio_outline_y_positions = [curve['y0'] for curve in page.curves if curve["height"] > 8]
+    if len(radio_outline_y_positions) == 0:
+        return None
+    radio_outline_y_positions.sort(reverse=True)
+    radio_check_y_positions = [curve['y0'] for curve in page.curves if curve["height"] < 8]
+    if len(radio_check_y_positions) > 1:
+        print("Too many checks.")
+        sys.exit()
+    checked_radio = find_checked_radio(radio_outline_y_positions, radio_check_y_positions[0])
+    return answers[checked_radio]
+
+
+def find_checked_radio(radio_outline_y_positions, radio_check_y_position):
+    nearest = None
+    distances = {}
+    for i, radio_outline in enumerate(radio_outline_y_positions):
+        distances[i] = abs(radio_outline - radio_check_y_position)
+        if nearest is None:
+            nearest = i
+        else:
+            if distances[i] < distances[nearest]:
+                nearest = i
+    return nearest
+
+
+arguments = parse_arguments()
+create_output_directory(arguments['output_file'])
+parse_pdf(arguments['input_file'], arguments['output_file'])
 
 print("Done")
