f6e72048488ea69d735845a15d1a88d62aee46b4
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-10-04 15:35:07 +0200

    Fix tests

--- test/test-parse.sh
+++ test/test-parse.sh
@@ -7,17 +7,17 @@ LC_CTYPE=en_US.UTF-8
 LC_ALL=en_US.UTF-8
 
 TEST_RESOURCES="../resources/test/"
-ACTUAL_RESULT_FILE="./output/actual-result.json"
+RESULT_DIR="./output/"
 EXPECTED_RESULT_FILE="./expected-result.json"
 
 SCRIPT_DIR="$(dirname "$0")"
 
-"$SCRIPT_DIR/../scripts/parse.sh" -i "$TEST_RESOURCES" -o "$ACTUAL_RESULT_FILE" -f "json" -n -c
+"$SCRIPT_DIR/../scripts/parse.sh" -i "$TEST_RESOURCES" -o "$RESULT_DIR" -c
 
 if command -v colordiff &>/dev/null; then
-    DIFF=$(colordiff --side-by-side <(jq . $ACTUAL_RESULT_FILE) <(jq . $EXPECTED_RESULT_FILE))
+    DIFF=$(colordiff --side-by-side <(jq . "$RESULT_DIR/parsed-data.json") <(jq . $EXPECTED_RESULT_FILE))
 else
-    DIFF=$(diff --side-by-side <(jq . $ACTUAL_RESULT_FILE) <(jq . $EXPECTED_RESULT_FILE))
+    DIFF=$(diff --side-by-side <(jq . "$RESULT_DIR/parsed-data.json") <(jq . $EXPECTED_RESULT_FILE))
 fi
 
 echo ""
